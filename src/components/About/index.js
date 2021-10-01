18055ba18c3d1fd615df865bff52d22acb1439d9
friel2021 <friel2009@gmail.com>
2021-10-01 12:43:26 -0600

    added about/index.js

---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="container">
+      <h2 class="top-title">Gregory Friel</h2>
+      <hr></hr>
+      <div>
+        <img
+          class="mb-5"
+          id="avatar"
+          src="https://friel2021.github.io/Portfolio/Assets/images/Gregphoto.jpg"
+          alt="avatar image"
+        />
+
+        <p>
+          A self titled IT guru and newly aspiring Web Developer. I work best
+          under pressure and look forward to meeting any and all challenges
+          head-on. Being busy is my forte and I enjoy all things computer
+          related. My previous work experience is in Sales and IT / Help Desk
+          assistance. I have an A+ Certification and now looking forward to
+          moving from the hardware side of the things to the software side!
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
